fb09881b9300ad6b49132a9c8144ecc23795e6ef
Deepakraja03 <12306deepakraja@gmail.com>
2023-03-06 15:29:03 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [rollNo, setRollNo] = useState('');
+    const [name, setName] = useState('');
+    const [students, setStudents] = useState([]);
+    const handleDelete =(idToDelete) => {
+      setStudents(students.filter(student => student.id !== idToDelete));
+    };
+  
+    useEffect(() => {
+      const storedStudents = localStorage.getItem('students');
+      if (storedStudents) {
+        setStudents(JSON.parse(storedStudents));
+      }
+    }, []);
+  
+    useEffect(() => {
+      localStorage.setItem('students', JSON.stringify(students));
+    }, [students]);
+  
+    function handleSubmit(event) {
+      event.preventDefault();
+      setStudents([...students, { rollNo, name }]);
+      setRollNo('');
+      setName('');
+    }
+  
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="rollNo">Roll No:</label>
+          <input
+            type="text"
+            id="rollNo"
+            value={rollNo}
+            onChange={(event) => setRollNo(event.target.value)}
+          />
+  
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+  
+          <button type="submit">Add student</button>
+        </form>
+  
+        <h2>Students:</h2>
+        <ul>
+          {students.map((student) => (
+            <li key={student.rollNo}>
+              {student.rollNo} - {student.name}
+              <button onClick={() => handleDelete(student.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  
 
 export default App;
+
